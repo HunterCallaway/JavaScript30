b8c0d7b3dee4d3d2cfa11eba6727f1dd57274dce
Hunter Callaway <HunterCallaway@hotmail.com>
2019-03-26 23:08:34 -0600

    Added querySelectors for the audio files and the key class. Wrote a conditional in case a non-home key is pressed. Used built-in functions for the audio and key variables.

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,4 +1,10 @@
 window.addEventListener("keydown", function(e) {
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-	console.log(audio);
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	//Stops the function from running.
+	if(!audio) return;
+	//Restarts the audio.
+	audio.currentTime = 0;
+	audio.play();
+	key.classList.add('playing');
 });
