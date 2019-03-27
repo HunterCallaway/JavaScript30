6a66a57ea606f500a962bda16ce960d0285ad48d
Hunter Callaway <HunterCallaway@hotmail.com>
2019-03-26 23:44:00 -0600

    Moved the function inside of the addEventListener function outside of the parent function. Worked on the removeTransition function.

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,10 +1,21 @@
-window.addEventListener("keydown", function(e) {
-	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-	//Stops the function from running.
-	if(!audio) return;
-	//Restarts the audio.
-	audio.currentTime = 0;
-	audio.play();
-	key.classList.add('playing');
-});
+function playSound(event) {
+		const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+		const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+		//Stops the function from running.
+		if(!audio) return;
+		//Restarts the audio.
+		audio.currentTime = 0;
+		audio.play();
+		key.classList.add('playing');
+}
+
+function removeTransition(event) {
+	//Skip it if it is not a transform.
+	if(event.propertyName !== 'transform') return;
+	this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener("keydown", playSound);
