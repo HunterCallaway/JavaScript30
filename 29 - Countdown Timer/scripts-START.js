5be4ec07d1d3c78bb2d3777c2ae37255f8f22d91
Hunter Callaway <HunterCallaway@hotmail.com>
2019-04-26 16:05:26 -0600

    Added endTime and buttons variables. Created a displayEndTime() function.

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,10 +1,13 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
 	const now = Date.now();
 	const then = now + seconds * 1000;
 	displayTimeLeft(seconds);
+	displayEndTime(then);
 
 	countdown = setInterval(() => {
 		const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -25,4 +28,11 @@ function displayTimeLeft(seconds) {
 	timerDisplay.textContent = display;
 	document.title = display;
 	console.log({minutes, remainderSeconds});
+}
+
+function displayEndTime(timestamp) {
+	const end = new Date(timestamp);
+	const hour = end.getHours();
+	const minutes = end.getMinutes();
+	endTime.textContent = `Be back at ${hour > 12 ? hour - 12 : hour}:${minutes < 10 ? '0' : '' }${minutes}`;
 }
